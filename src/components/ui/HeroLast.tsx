ff2c0e38975cdf29fbf00d1ff447f88438289873
isiagi <isiagigeofrey0@gmail.com>
2022-01-16 06:18:01 +0300

    add animation

--- src/components/ui/HeroLast.tsx
+++ src/components/ui/HeroLast.tsx
@@ -1,13 +1,19 @@
+import {useEffect} from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import img from '../images/3.jpg'
 import './hero.css'
 
 const HeroLast = () => {
+  useEffect(() => {
+    AOS.init({duration: 2000})
+})
   return (
     <div className="hero-middle">
-      <div>
+      <div data-aos="fade-up">
         <img src={img} alt="hey" />
       </div>
-      <div>
+      <div data-aos="fade-down">
         <h2>Patricia Outstanding</h2>
         <p>Practical and elegant to match the new band handle.</p>
         <button className="btn">Shop now</button>
--- src/components/ui/HeroMiddle.tsx
+++ src/components/ui/HeroMiddle.tsx
@@ -1,16 +1,21 @@
-
+import {useEffect} from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import img from '../images/4.jpg'
 import './hero.css'
 
 const HeroMiddle = () => {
+    useEffect(() => {
+        AOS.init({duration: 2000})
+    })
     return (
         <div className="hero-middle">
-            <div>
+            <div data-aos="fade-right">
                 <h2>Patricia Outstanding</h2>
                 <p>Practical and elegant to match the new band handle.</p>
                 <button className="btn">Shop now</button>
             </div>
-            <div>
+            <div data-aos="fade-left">
                 <img src={img} alt="hey" />
             </div>
         </div>
